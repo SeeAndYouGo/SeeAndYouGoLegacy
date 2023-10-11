a60e0b6f2f4b1eabc133edeef66749840bbb08af
skj4244 <skj4244@naver.com>
2023-10-11 13:42:10 +0900

    fix: Menu scheduler

--- backend/src/main/java/com/seeandyougo/seeandyougo/service/IterService.java
+++ backend/src/main/java/com/seeandyougo/seeandyougo/service/IterService.java
@@ -33,7 +33,7 @@ public class IterService {
     }
 
     @Transactional
-    @Scheduled(cron="0 0 0 * * *")
+    @Scheduled(cron="0 0 0 * * *", initialDelay = 1000)
     public void repeatCallMenu() throws Exception { // 하루에 한번씩 갱신하느건??
         rawMenuService.saveTodayMenu();
         cashService.menuTodayCashing();
